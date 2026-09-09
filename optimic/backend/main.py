937de7bf2cf2d57fe5802a9bfaee563077b2ee77
KaraniAbdellah <abdellahkarani@gmail.com>
2026-09-09 20:20:42 +0100

    feat: deploy backend to GCP

--- optimic/backend/main.py
+++ optimic/backend/main.py
@@ -27,6 +27,9 @@ app = FastAPI()
 origins = [
     "http://localhost:5173/",
     "http://127.0.0.1:5173/",
+    "https://optimic.vercel.app/"
+    "https://optimic.vercel.app"
+    "http://optimic.vercel.app"
 ]
 
 
